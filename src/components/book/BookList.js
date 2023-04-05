671e6c93a42a44e4215479b1008c57122f1bee29
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-05 16:15:47 +0500

    prop values as variables in book list component

--- src/components/book/BookList.js
+++ src/components/book/BookList.js
@@ -1,11 +1,30 @@
 import Book from "./Book";
 
 function BookList(){
-    return(
+
+    const firstBook = {
+        image: './so_good.jpg',
+        title : "So good they can't ignore you",
+        author: "Cal Newport"
+    }
+
+    const secondBook = {
+        image: './gift_of_done.jpg',
+        title : "Finish - give yourself the gift of done",
+        author: "Jon Acuff"
+    }
+
+    const thirdBook = {
+        image: './cracking.jpg',
+        title : "Cracking the coding interview",
+        author: "Gayle Laakman Mcdowell"
+    }
+
+    return (
         <section className={"booklist"}>
-            <Book image={'./so_good.jpg'} title={"So good they can't ignore you"} author={"Cal Newport"}/>
-            <Book image={'./gift_of_done.jpg'} title={"Finish - give yourself the gift of done"} author={"Jon Acuff"}/>
-            <Book image={'./cracking.jpg'} title={'Cracking the coding interview'} author={"Gayle Laakman Mcdowell"}/>
+            <Book image={firstBook.image} title={firstBook.title} author={firstBook.author}/>
+            <Book image={secondBook.image} title={secondBook.title} author={secondBook.author}/>
+            <Book image={thirdBook.image} title={thirdBook.title} author={thirdBook.author}/>
         </section>
     );
 }
