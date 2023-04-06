09794cdb20db4cac4204a12f904ffb2518ec829f
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-06 11:00:12 +0500

    render list and object passing one by one to child property

--- src/components/book/BookList.js
+++ src/components/book/BookList.js
@@ -1,30 +1,12 @@
 import Book from "./Book";
 
-function BookList(){
-
-    const firstBook = {
-        image: './so_good.jpg',
-        title : "So good they can't ignore you",
-        author: "Cal Newport"
-    }
-
-    const secondBook = {
-        image: './gift_of_done.jpg',
-        title : "Finish - give yourself the gift of done",
-        author: "Jon Acuff"
-    }
-
-    const thirdBook = {
-        image: './cracking.jpg',
-        title : "Cracking the coding interview",
-        author: "Gayle Laakman Mcdowell"
-    }
+function BookList(props){
 
     return (
         <section className={"booklist"}>
-            <Book image={firstBook.image} title={firstBook.title} author={firstBook.author}/>
-            <Book image={secondBook.image} title={secondBook.title} author={secondBook.author}/>
-            <Book image={thirdBook.image} title={thirdBook.title} author={thirdBook.author}/>
+            {props.books.map(book =>
+                <Book {...book} />)
+            }
         </section>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import React from 'react';
 import BookList from "./components/book/BookList";
 
 import './index.css';
 
+const firstBook = {
+    id: 1,
+    image: './so_good.jpg',
+    title : "So good they can't ignore you",
+    author: "Cal Newport"
+}
+
+const secondBook = {
+    id: 2,
+    image: './gift_of_done.jpg',
+    title : "Finish - give yourself the gift of done",
+    author: "Jon Acuff"
+}
+
+const thirdBook = {
+    id: 3,
+    image: './cracking.jpg',
+    title : "Cracking the coding interview",
+    author: "Gayle Laakman Mcdowell"
+}
+let books = [firstBook, secondBook, thirdBook];
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<BookList />);
+root.render(<BookList books={books}/>);
