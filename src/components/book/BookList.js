d0ad7df1fa7d5a28eddc2230804eb9a12414bc01
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-06 15:52:58 +0500

    react basic chapter completed

--- src/components/book/BookList.js
+++ src/components/book/BookList.js
@@ -4,15 +4,18 @@ import EventExamples from "./EventExamples";
 function BookList(props){
 
     return (
-        <section className={"booklist"}>
-            <EventExamples />
-            {props.books.map((book, index) =>
-                <Book title={book.title} image={book.image}
-                      author={book.author} key={book.id}
-                        number={index}
-                />)
-            }
-        </section>
+        <>
+            <h2>Amazon best sellers</h2>
+            <section className={"booklist"}>
+                <EventExamples />
+                {props.books.map((book, index) =>
+                    <Book title={book.title} image={book.image}
+                          author={book.author} key={book.id}
+                          number={index}
+                    />)
+                }
+            </section>
+        </>
     );
 }
 
